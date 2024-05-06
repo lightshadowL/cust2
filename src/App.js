96999a8950bfc682c805122be2c6d47d1c86ee6c
lee <lsljw65@gmail.com>
2024-05-06 15:36:24 +0900

    class props와 map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Customer from './components/Customer';
+
+const customers=[
+  {
+  'id':1,
+  'image':'/img/new-s-1.jpg',
+  'name':'홍길숙',
+  'birthday':'961222',
+  'gender':'남성',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image':'/img/new-s-2.jpg',
+  'name':'이길동',
+  'birthday':'900101',
+  'gender':'남성',
+  'job':'회사원'
+},
+{
+  'id':3,
+  'image':'/img/new-s-3.jpg',
+  'name':'김주부',
+  'birthday':'850205',
+  'gender':'여성',
+  'job':'주부'
+}
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo"></img>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c =>{
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
+    
   );
 }
 
